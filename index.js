e0c6d531d4b0e7c46029657a888c6ab7a0b6cf11
ferry3213 <65997639+ferry3213@users.noreply.github.com>
2022-09-02 19:21:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ async function startHisoka() {
 	for (let i of kon) {
 	    list.push({
 	    	displayName: await hisoka.getName(i + '@s.whatsapp.net'),
-	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await hisoka.getName(i + '@s.whatsapp.net')}\nFN:${await hisoka.getName(i + '@s.whatsapp.net')}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:minin@my.id\nitem2.X-ABLabel:Email\nitem3.URL:https://mininproject.dev\nitem3.X-ABLabel:-\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await hisoka.getName(i + '@s.whatsapp.net')}\nFN:${await hisoka.getName(i + '@s.whatsapp.net')}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:ferryisnawan9@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://instagram.com/ferry_isnawan45\nitem3.X-ABLabel:-\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`
 	    })
 	}
 	hisoka.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
